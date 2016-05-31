5027455193a1b9a7824df29e9b3c43a00ece83a9
knt5 <knt5@users.noreply.github.com>
2016-05-31 23:45:13 +0900

    Add to create input stream

---
+++ example/convert-tokyo.js
@@ -0,0 +1,3 @@
+const convert = require('../src/jpgis-convert');
+
+convert('../data/gsi-tokyo/building-peripheral-line/FG-GML-533946-11-20151001/FG-GML-533946-BldA-20151001-0001.xml');
--- src/jpgis-convert.js
+++ src/jpgis-convert.js
@@ -1,14 +1,30 @@
-var fs = require('fs');
+const fs = require('fs');
+const readline = require('readline');
 
 var convert = (filePath, options) => {
 	
+	var rl = readline.createInterface({
+		input: fs.ReadStream(filePath),
+		output: null
+	});
 	
+	rl.on('line', (line) => {
+		
+		if (line.substr(0, 5) === '<BldA') {
+			
+			console.log('***' + line + '***');
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	});
 	
-	
-	
-	
-	
-	
+	rl.resume();
 };
 
 module.exports = convert;
