25a94999ceb341823e0177b07a63920dca7c5509
knt5 <knt5@users.noreply.github.com>
2016-05-31 22:32:29 +0900

    Update watch task

--- gulp/task/watch.js
+++ gulp/task/watch.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
-var config = require('../config/config');
+var config = require('../config');
 
 gulp.task('watch', function() {
-	
+	gulp.watch(config.lint.js.path, ['lint:js']);
 });
