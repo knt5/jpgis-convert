ec95d427c3be055ceb23cba12d271cef8889b898
knt5 <knt5@users.noreply.github.com>
2016-05-31 22:37:20 +0900

    Update lint task

--- gulp/task/lint.js
+++ gulp/task/lint.js
@@ -1,13 +1,11 @@
-/*
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var eslint = require('gulp-eslint');
-var config = require('../config/config');
-*/
+var config = require('../config');
 
 // JavaScript lint
 gulp.task('lint:js', function() {
-	return gulp.src(config.lint.targets.js)
+	return gulp.src(config.lint.js.path)
 	.pipe(plumber())
 	.pipe(eslint(config.eslint))
 	.pipe(eslint.format())
