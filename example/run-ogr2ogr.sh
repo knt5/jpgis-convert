0d28613f9184133834c1d6c7fc2e507adf5542cc
knt5 <knt5@users.noreply.github.com>
2016-06-03 23:31:52 +0900

    Change area size to extract and simplification parameter of ogr2ogr

--- example/run-ogr2ogr.sh
+++ example/run-ogr2ogr.sh
@@ -5,11 +5,11 @@ name=533946
 mkdir shape
 
 ogr2ogr \
-	-where 'OGR_GEOM_AREA > 0.000000007' \
+	-where 'OGR_GEOM_AREA > 0.0000000025' \
 	-lco ENCODING=UTF-8 \
 	-s_srs EPSG:4612 \
 	-t_srs EPSG:4326 \
 	-f 'ESRI Shapefile' \
-	-simplify 0.00001 \
+	-simplify 0.000005 \
 	"shape/$name".shp \
 	"$name".geojson
