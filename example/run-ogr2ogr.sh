55925995e4b265202afb0cddec10c6350bfe8a1e
knt5 <knt5@users.noreply.github.com>
2016-06-04 14:45:06 +0900

    Update GeoJSON to Shape converter to be able to convert multiple files

--- example/run-ogr2ogr.sh
+++ example/run-ogr2ogr.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-name=533946
-
 mkdir shape
 
-ogr2ogr \
-	-where 'OGR_GEOM_AREA > 0.0000000025' \
-	-lco ENCODING=UTF-8 \
-	-s_srs EPSG:4612 \
-	-t_srs EPSG:4326 \
-	-f 'ESRI Shapefile' \
-	-simplify 0.000005 \
-	"shape/$name".shp \
-	"$name".geojson
+\ls *.geojson | while read line
+do
+	name=`basename $line .geojson`
+	
+	ogr2ogr \
+		-where 'OGR_GEOM_AREA > 0.0000000025' \
+		-lco ENCODING=UTF-8 \
+		-s_srs EPSG:4612 \
+		-t_srs EPSG:4326 \
+		-f 'ESRI Shapefile' \
+		-simplify 0.000005 \
+		"shape/$name".shp \
+		"$name".geojson
+done
