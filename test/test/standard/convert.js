7ccb74da66bbea8e41fbdfd5346d27765348963b
knt5 <knt5@users.noreply.github.com>
2016-10-29 15:10:59 +0900

    Add test code

---
+++ test/test/standard/convert.js
@@ -0,0 +1,44 @@
+require('helper');
+
+const typeId = {
+	'普通建物': 0,
+	'堅ろう建物': 1,
+	'普通無壁舎': 2,
+	'堅ろう無壁舎': 3
+};
+
+describe('Convert standard files', () => {
+	it('Convert with standard settings', (done) => {
+		const input = './test/data/input.xml';
+		const output = './test/work/data.geojson';
+		const expected = './test/data/expectedData.geojson';
+		
+		convert([input], {
+			output,
+			typeId
+		}, () => {
+			expect(fs.readFileSync(output).toString())
+				.to.equal(fs.readFileSync(expected).toString());
+			done();
+		});
+	});
+	
+	it('Convert with ignoreTypes setting', (done) => {
+		const input = './test/data/input.xml';
+		const output = './test/work/dataWithIgnoreTypes.geojson';
+		const expected = './test/data/expectedDataWithIgnoreTypes.geojson';
+		
+		convert([input], {
+			output,
+			typeId,
+			ignoreTypes: new Set([
+				'普通建物',
+				'普通無壁舎'
+			])
+		}, () => {
+			expect(fs.readFileSync(output).toString())
+				.to.equal(fs.readFileSync(expected).toString());
+			done();
+		});
+	});
+});
