77df2a5f521be9793ebf7d0e9efa054ac642c9a4
knt5 <knt5@users.noreply.github.com>
2016-10-29 16:17:45 +0900

    Update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,31 +9,21 @@ module.exports = {
 		'eslint-config-airbnb-base/rules/es6',
 		// no "rules/imports"
 	],
+	env: {
+		node: true,
+		mocha: true,
+	},
+	globals: {
+		convert: true,
+		expect: true,
+	},
 	rules: {
-		'camelcase': 'error',
-		'indent': ['error', 'tab'],
+		camelcase: 'error',
+		'func-names': 'off',
+		indent: ['error', 'tab'],
+		'no-console': 'off',
+		'no-plusplus': 'off',
 		'no-tabs': 'off',
 		'no-use-before-define': 'off',
-		'no-plusplus': 'off',
-		/*
-		'comma-spacing': [2, {'before': false, 'after': true}],
-		'comma-style': [2, 'last'],
-		'computed-property-spacing': [2, 'never'],
-		'eol-last': 2,
-		'key-spacing': [2, {'beforeColon': false, 'afterColon': true}],
-		'keyword-spacing': 2,
-		'linebreak-style': [2, 'unix'],
-		'max-len': [2, {'code': 120, 'comments': 180, 'ignoreUrls': true}],
-		'no-alert': 2,
-		'no-console': 1,
-		'no-spaced-func': 2,
-		'no-trailing-spaces': [2, { 'skipBlankLines': true }],
-		'no-unused-vars': 1,
-		'no-whitespace-before-property': 2,
-		'quotes': [2, 'single'],
-		'semi': [2, 'always'],
-		'semi-spacing': [2, { 'before': false, 'after': true }],
-		'space-before-blocks': [2, 'always']
-		*/
 	}
 };
