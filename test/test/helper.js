80ca2de628704c3af279ce0290f547d7b8319d1b
knt5 <knt5@users.noreply.github.com>
2016-10-29 17:54:38 +0900

    Add removeFile() to helper

--- test/test/helper.js
+++ test/test/helper.js
@@ -1,2 +1,12 @@
 global.expect = require('chai').expect;
 global.convert = require('../../');
+
+global.removeFile = (path) => {
+	try {
+		fs.accessSync(path);
+	} catch (e) {
+		return;
+	}
+	
+	fs.unlinkSync(path);
+};
